24ba47af294226ec967eae594dcb3263787c21b7
Mithunavannan <madhansurya2004@gmail.com>
2026-03-07 14:04:48 +0000

    Sync LeetCode submission - Permutation in String (python)

---
+++ problems/permutation_in_string/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        from collections import Counter
+
+        s1_count = Counter(s1)
+        window_count = Counter()
+        left = 0
+
+        for right in range(len(s2)):
+            window_count[s2[right]] += 1
+
+            if right - left + 1 > len(s1):
+                window_count[s2[left]] -= 1
+                if window_count[s2[left]] == 0:
+                    del window_count[s2[left]]
+                left += 1
+            if window_count ==  s1_count:
+                return True
+        return False
