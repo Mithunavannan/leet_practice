2327e334ad92b7087dbccbea96cf79dfb08b3c95
Mithunavannan <madhansurya2004@gmail.com>
2026-03-16 05:35:17 +0000

    Sync LeetCode submission - House Robber (python)

---
+++ problems/house_robber/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rob(self, nums):
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+        nums[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            nums[i] = max(nums[i-1],nums[i] + nums[i-2])
+        return nums[-1]
