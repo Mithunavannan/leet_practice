15f8406b4e63b343fbad126b5cd9586f49dc625d
Mithunavannan <madhansurya2004@gmail.com>
2026-03-27 13:39:45 +0000

    Sync LeetCode submission - Max Area of Island (python)

---
+++ problems/max_area_of_island/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        if not grid:
+            return 0
+        max_area = 0
+        
+        def dfs(grid, i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            area = 1
+            area += dfs(grid, i+1, j)
+            area += dfs(grid, i-1, j)
+            area += dfs(grid, i, j+1)
+            area += dfs(grid, i, j-1)
+            return area
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, dfs(grid, i, j))
+        return max_area
