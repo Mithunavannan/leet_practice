20f4919e407b7e1697b8aaca40e682f99a70b76f
Mithunavannan <madhansurya2004@gmail.com>
2026-05-28 20:30:07 +0000

    Sync LeetCode submission - Daily Temperatures (python)

---
+++ problems/daily_temperatures/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        answer = [0]*n
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index
+
+            stack.append(i)
+        return answer
