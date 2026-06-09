250c9be5d07a21a87c83a925689df4b82e1d3b18
Mithunavannan <madhansurya2004@gmail.com>
2026-06-09 06:09:47 +0000

    Sync LeetCode submission - Time Based Key-Value Store (python)

---
+++ problems/time_based_key-value_store/solution.py
@@ -0,0 +1,36 @@
+class TimeMap(object):
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key, value, timestamp):
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([timestamp, value])
+        
+
+    def get(self, key, timestamp):
+        if key not in self.store:
+            return ""
+        values = self.store[key]
+
+        left = 0
+        right = len(values) - 1
+
+        result = ""
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if values[mid][0] <= timestamp:
+                result = values[mid][1]
+                left = mid + 1
+            else:
+                right = mid - 1
+        return result
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
