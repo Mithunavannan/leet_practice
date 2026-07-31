fd76fa1601fb1b3f1669584ce85153a10a41ad50
Mithunavannan <madhansurya2004@gmail.com>
2026-07-31 09:45:42 +0000

    Sync LeetCode submission - String Compression (python)

---
+++ problems/string_compression/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def compress(swself, chars):
+        write = 0
+        i = 0
+
+        while i < len(chars):
+            current = chars[i]
+            count = 0
+
+            while i < len(chars) and chars[i] == current:
+                count += 1
+                i += 1
+
+            chars[write] = current
+            write += 1
+
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+        return write
